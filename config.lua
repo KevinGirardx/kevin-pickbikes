fc3485d778b66037a80ed8cf2a64d2316d5fccbf
KamuiKody <62488881+KamuiKody@users.noreply.github.com>
2022-08-13 05:27:11 -0400

    Per Bike Positioning
    easily config the position and rotation of each bicycle individually

--- config.lua
+++ config.lua
@@ -1,14 +1,63 @@
 Config = Config or {}
 
-Config.Interaction = "qb" --qb/target (you dont need to put  target if you want to use target, can just leave blank)
+Config.Interaction = "" --qb/target (you dont need to put  target if you want to use target, can just leave blank)
 Config.InteractKey = 'G'
 Config.DrawTextPosition = "left"
 Config.Bikes = {
-    `bmx`,
-    `tribike3`,
-    `fixter`,
-    `cruiser`,
-    `scorcher`,
-    `tribike2`,
-    `tribike`,
+    ['bmx'] = {
+        ['x'] = 0.0,
+        y = 0.35,
+        z = 0.160,
+        RotX = 180.0,
+        RotY = 170.0,
+        RotZ = 90.0
+    },
+    ['tribike3'] = {
+        x = 0.0,
+        y = 0.35,
+        z = 0.160,
+        RotX = 180.0,
+        RotY = 170.0,
+        RotZ = 90.0
+    },
+    ['fixter'] = {
+        x = 0.0,
+        y = 0.35,
+        z = 0.160,
+        RotX = 180.0,
+        RotY = 170.0,
+        RotZ = 90.0
+    },
+    ['cruiser'] = {
+        x = 0.0,
+        y = 0.35,
+        z = 0.160,
+        RotX = 180.0,
+        RotY = 170.0,
+        RotZ = 90.0
+    },
+    ['scorcher'] = {
+        x = 0.0,
+        y = 0.35,
+        z = 0.160,
+        RotX = 180.0,
+        RotY = 170.0,
+        RotZ = 90.0
+    },
+    ['tribike2'] = {
+        x = 0.0,
+        y = 0.35,
+        z = 0.160,
+        RotX = 180.0,
+        RotY = 170.0,
+        RotZ = 90.0
+    },
+    ['tribike'] = {
+        x = 0.0,
+        y = 0.35,
+        z = 0.160,
+        RotX = 180.0,
+        RotY = 170.0,
+        RotZ = 90.0
+    },
 }
